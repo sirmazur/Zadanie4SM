0b3143cbec16c2c323a2fa1c58b8f348bbf15e4d
Piotr Mazur <84686321+sirmazur@users.noreply.github.com>
2023-11-05 16:58:02 +0100

    Add files via upload

---
+++ Zadanie4SM/zadanie4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zadanie4
+//
+//  Created by Piotr Mazur on 04/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
